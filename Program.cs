959aabfcb0b30c47f98659cd6c9ccd821f708077
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2019-05-04 17:35:21 -0300

    Project created 🎉

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace Simple_ClientSide_Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
